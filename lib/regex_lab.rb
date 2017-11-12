2661c70655c652b2f3ea7743ef95498c2752188b
Sherry <sherryptk@gmail.com>
2017-11-12 17:55:07 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,6 @@
 def starts_with_a_vowel?(word)
-   word.match(/\b[aeiouAEIOU]/) == nil ? false:true
+  #  word.match(/\b[aeiouAEIOU]/) == nil ? false:true
+  word.scan(/\b[aeiouAEIOU]/) ? true:false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
