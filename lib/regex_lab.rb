9a6145ab85fc5f28979f57794eb98019c6962455
Sherry <sherryptk@gmail.com>
2017-11-12 19:00:37 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.match(/\b\d{10}.*\b/) == nil ? false:true
+  phone.match(/\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\z/) == nil ? false:true
 end
